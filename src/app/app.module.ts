76e9d95e0b800aaeccf9a57afc72b4f5cce67d54
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-08-23 17:58:10 +0200

    added main header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ComponentsModule } from './components/components.module';
 import { PagesModule } from './pages/pages.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, PagesModule],
+  imports: [AppRoutingModule, ComponentsModule, PagesModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { HeaderComponent } from './header/header.component';
 import { UsersButtonsContainerModule } from './users-buttons-container/users-buttons-container.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent],
   imports: [CommonModule, UsersButtonsContainerModule],
-  exports: [UsersButtonsContainerModule],
+  exports: [HeaderComponent, UsersButtonsContainerModule],
 })
 export class ComponentsModule {}
