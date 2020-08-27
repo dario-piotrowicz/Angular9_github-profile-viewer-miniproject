ed9d5d75eae4fc35ab34c86d774c0fcdb1687e16
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-08-27 19:48:36 +0200

    renamed home component to home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 
 import { RouterModule, Routes } from '@angular/router';
 
-import { HomeComponent } from './pages/home/home.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 import { PagesModule } from './pages/pages.module';
 
 const routes: Routes = [
   {
     path: '',
-    component: HomeComponent,
+    component: HomePageComponent,
   },
 ];
 
---
+++ src/app/pages/home-page/home-page.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { Observable, of, timer } from 'rxjs';
+import { UserSearchDetails } from 'src/app/types/userBasicDetails';
+import { UserDetailsService } from 'src/app/services/user-details/user-details.service';
+import { switchMap, tap, finalize, delay } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.sass'],
+})
+export class HomePageComponent {
+  public query = '';
+  public usersData$: Observable<UserSearchDetails[]>;
+  public loadingResults = false;
+
+  constructor(private userBasicDetailsService: UserDetailsService) {}
+
+  public searchUsers(): void {
+    if (!this.query || this.loadingResults) {
+      return;
+    }
+
+    this.loadingResults = true;
+    this.usersData$ = of(null).pipe(
+      switchMap(() => this.userBasicDetailsService.searchForUsers(this.query)),
+      delay(700),
+      finalize(() => (this.loadingResults = false))
+    );
+  }
+}
