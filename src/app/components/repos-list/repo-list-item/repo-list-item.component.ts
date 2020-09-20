2da1363b44e22b0b5eb29cec18fe8d035ffe7710
Dario Piotrowicz <dario.piotrowicz@gmail.com>
2020-09-20 20:32:09 +0200

    repo-list-item showing fork icon if repo is a fork (instead of using text)

--- src/app/components/repos-list/repo-list-item/repo-list-item.component.ts
+++ src/app/components/repos-list/repo-list-item/repo-list-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { Repo } from 'src/app/types/repositories';
+import { faCodeBranch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-repo-list-item',
@@ -10,4 +11,6 @@ export class RepoListItemComponent {
   @Input() repo: Repo;
 
   public readonly dateFormat = 'MMM dd yyyy - HH:mm';
+
+  public readonly forkFaIcon = faCodeBranch;
 }
--- src/app/components/repos-list/repos-list.module.ts
+++ src/app/components/repos-list/repos-list.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { ReposListComponent } from './repos-list.component';
 import { RepoListItemComponent } from './repo-list-item/repo-list-item.component';
 
 @NgModule({
   declarations: [ReposListComponent, RepoListItemComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   exports: [ReposListComponent],
 })
 export class ReposListModule {}
